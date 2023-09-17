06083538d8862f4093a18d8aa91695826bab2f20
luisvls <luisvls88@gmail.com>
2023-09-16 21:48:44 -0300

    Updates

---
+++ assets/main.js
@@ -0,0 +1,50 @@
+const API =
+  "https://youtube-v31.p.rapidapi.com/search?channelId=UCrT41pW5aiE6F0K8aqNxuzQ&part=snippet%2Cid&order=date&maxResults=9";
+
+const content = null || document.getElementById("content");
+
+const options = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "80d62098d7mshf6fa1a2cd24e78bp1f38d8jsn4e6e198dea9f",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+async function fetchData(urlApi) {
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items
+      .map(
+        (video) => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+    `
+      )
+      .slice(0, 4)
+      .join("")}
+
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+    console.error(error);
+  }
+})();
